397b39acb0bec230215dba2a8047b663d920bd41
gardenaldopcc <gardenaldopcc@gmail.com>
2026-08-17 04:44:16 +0000

    fewq

--- client/src/lib/siteConfig.ts
+++ client/src/lib/siteConfig.ts
@@ -605,7 +605,7 @@ export function getSiteConfig(): SiteConfig {
     const keys = Object.keys(CONFIGS);
     raw = CONFIGS[keys[keys.length - 1]] ?? DEFAULT_CONFIG;
   } else {
-    raw = CONFIGS[window.location.hostname] ?? CONFIGS[host] ?? DEFAULT_CONFIG;
+    raw = CONFIGS[window.location.hostname] ?? CONFIGS[host];
   }
   return { ...raw, siteName: raw.brand || raw.razaoSocial };
 }
